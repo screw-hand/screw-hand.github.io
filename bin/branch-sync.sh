1414157983e8e359e949bf781c69ea6c66ec4ae4
screw-hand <screwhand0@gmail.com>
2023-06-26 00:12:31 +0800

    hexo mkdir bin, append branch-sync shell

--- bin/branch-sync.sh
+++ bin/branch-sync.sh
@@ -15,18 +15,24 @@ TARGET_BRANCH="$2"
 if ! git show-ref --quiet refs/heads/$SOURCE_BRANCH; then
     echo "$SOURCE_BRANCH 分支不存在，正在检出..."
     git checkout -b $SOURCE_BRANCH
+    git branch -u origin/$SOURCE_BRANCH
 fi
 
 # 判断是否存在 TARGET_BRANCH 分支
 if ! git show-ref --quiet refs/heads/$TARGET_BRANCH; then
     echo "$TARGET_BRANCH 分支不存在，正在检出..."
     git checkout -b $TARGET_BRANCH
+    git branch -u origin/$TARGET_BRANCH
 fi
 
 git fetch
+
 git checkout $SOURCE_BRANCH
+git branch -u origin/$SOURCE_BRANCH
 git push
+
 git checkout $TARGET_BRANCH
+git branch -u origin/$TARGET_BRANCH
 git pull
 git merge origin/$SOURCE_BRANCH
 git push
